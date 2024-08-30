e1aaec8e5feab98c2c9f7b2ba1ecb0d29ec25329
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2024-08-31 00:35:38 +0500

    did android configuration to create a widget but its not completed yet

---
+++ android/app/src/main/java/com/videocallwidget/TodoAppWidgetProvider.java
@@ -0,0 +1,25 @@
+package com.videocallwidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class TodoAppWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.todo_app_widget);
+
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.todo_text, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
